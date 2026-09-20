c9132a77a76e300e2dd1911a31c9cd5171a72919
skhaw2004 <stuartkhaw@gmail.com>
2026-09-20 17:24:19 +0800

    Update tests and imports for remark field

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -9,8 +9,6 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
 import seedu.address.model.person.Person;
 
-import javax.swing.text.LabelView;
-
 /**
  * A UI component that displays information of a {@code Person}.
  */
